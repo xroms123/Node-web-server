ebaa92f3209d53b8f9ce2026427ca76abdda8fc1
Lou <kao@hiroia.com>
2018-08-16 14:57:52 +0800

    Node-web-server

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+let app = express();
+
+// set partials path
+hbs.registerPartials(__dirname+'/views/partials');
+
+app.set('view engine','hbs');
+
+// use middleware
+app.use((req,res,next)=>{
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`;
+    
+    // write log to server.lgo
+    fs.appendFile('server.log',log + '\n',(err)=>{
+        if(err){
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+})
+app.use((req,res,next)=>{
+    res.render('maintenance.hbs');
+})
+app.use(express.static(__dirname + '/public'));
+
+// set getCurrentYear and screamIt paramater
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear();
+})
+hbs.registerHelper('screamIt',(text)=>{
+    return text.toUpperCase();
+})
+
+// render home and about page
+app.get('/',(request,response)=>{
+    response.render('home.hbs',{
+        pageTitle:'Home Page',
+        welocomeMessage:'welcome to the website'
+    })
+});
+app.get('/about',(request,response)=>{
+    response.render('about.hbs',{
+        pageTitle:'About Page'
+    });
+});
+
+// setting port information
+app.listen(3000,()=>{
+    console.log('server is up on port 3000');
+});
